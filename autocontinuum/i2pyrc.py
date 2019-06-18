976b443671ba88068afae213a2d02ffa331797c3
Chris Howk <jchowk@gmail.com>
2019-06-18 12:44:04 -0400

    Added part config file.

---
+++ autocontinuum/i2pyrc.py
@@ -0,0 +1,4 @@
+# map_pro('nl_continuum_aod', inpars=[1],
+#     inkeys=['maxord,minord,nsig1,nsig2,vl1,vl2,vaod1,vaod2,contplot,aodplot,vclip1,blemish,vshift,flgaod,pix1,pix2,fmin,nocont,noerror,kind,pdfflg,changename'.split(',')]
+# map_pro('nl_do_cont_aod',
+#     inkeys=['filein,maxord,minord,nsig1,nsig2,vl1,vl2,vaod1,vaod2,contplot,aodplot,vclip1,blemish,vshift,flgaod,pix1,pix2,fmin,nocont,noerror,kind,pdfflg,changename'.split(',')]
