90ba6a9474738c153da43383f45bf4de9d290bb8
Sdeepak1997 <sdeepak@nd.edu>
2023-07-11 17:29:51 -0400

    Changed contin_order range
    Now checks between contin_order and contin_order+1 from rb_codes

--- pyNorm/io/pyn_io.py
+++ pyNorm/io/pyn_io.py
@@ -182,7 +182,7 @@ def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True):
         spec['v1'] = -100.
         spec['v2'] = +100.
 
-    spec = continuum_fit(spec,minord=spec['contin_order']-1,maxord=spec['contin_order']+1)
+    spec = continuum_fit(spec,minord=spec['contin_order'],maxord=spec['contin_order']+1)
     spec = pyn_batch(spec, verbose=False, partial_pixels=partial_pixels)
 
     return spec
