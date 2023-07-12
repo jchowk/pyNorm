31aea0ae202d1c48c915727ef4ccfae7d3b180f8
Sdeepak1997 <sdeepak@nd.edu>
2023-07-12 15:22:12 -0400

    Added a flag for blemishes in dictionary 'flag_blemish'

--- pyNorm/io/pyn_io.py
+++ pyNorm/io/pyn_io.py
@@ -10,7 +10,7 @@ def save_pyn_results(spec,filename = 'None'):
     with open(filename+'.p', "wb") as file:
         pickle.dump(spec, file)
 
-def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True):
+def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True, blemish_correction=True):
     import numpy as np
     from collections import OrderedDict
     from scipy.io import readsav
@@ -116,11 +116,12 @@ def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True):
     spec['contin_mask_bits'] = np.zeros(len(spec_in['wc'])) #True = included
     spec['contin_mask_bits'][masked_area] = 0
     spec['contin_mask_bits'][~masked_area] = 1
-    spec['contin_mask_bits'][spec['flux']<0] = 0 #this masks the blemishes
+    spec['contin_mask_bits'][spec['flux']<0] = 0 
     spec['mask_cont'] = np.zeros(len(spec_in['wc']))
     spec['mask_cont'][masked_area] = 0
     spec['mask_cont'][~masked_area] = 1
-    spec['mask_cont'][spec['flux']<0] = 0 #this masks the blemishes
+    spec['mask_cont'][spec['flux']<0] = 0      
+                          # added by saloni lines 122,123
     spec = __convert_inorm_mask(spec)
     #
     # Normalized spectrum
@@ -168,7 +169,6 @@ def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True):
     spec['ncol_err_lo'] = spec_in['Nsig']
     spec['ncol_err_hi'] = spec_in['Nsig']
     spec['flag_sat'] = False
-    # added a flag for blemishes
     spec['flag_blemish'] = False
     #
     spec['va'] = 0.0#spec_in['va'] #average velocity
@@ -187,11 +187,11 @@ def read_rbcodes(input_filename, targname, ra, dec, ion, partial_pixels=True):
         spec['v2'] = +100.
 
     spec = continuum_fit(spec,minord=spec['contin_order'],maxord=spec['contin_order']+1)
-    spec = pyn_batch(spec, verbose=False, partial_pixels=partial_pixels)
+    spec = pyn_batch(spec, verbose=False, partial_pixels=partial_pixels,blemish_correction=blemish_correction)
 
     return spec
 
-def read_inorm(input_filename, partial_pixels=True):
+def read_inorm(input_filename, partial_pixels=True, blemish_correction=True):
     import numpy as np
     from collections import OrderedDict
     from scipy.io import readsav
@@ -325,8 +325,6 @@ def read_inorm(input_filename, partial_pixels=True):
     spec['ncol_err_lo'] = spec_in['ncole1']
     spec['ncol_err_hi'] = spec_in['ncole2']
     spec['flag_sat'] = False
-    # added a flag for blemishes
-    spec['flag_blemish'] = False
     #
     spec['va'] = spec_in['va']
     spec['va_err'] = spec_in['vaerr']
@@ -343,7 +341,7 @@ def read_inorm(input_filename, partial_pixels=True):
         spec['v1'] = -100.
         spec['v2'] = +100.
 
-    spec = pyn_batch(spec, verbose=False, partial_pixels=partial_pixels)
+    spec = pyn_batch(spec, verbose=False, partial_pixels=partial_pixels, blemish_correction=blemish_correction)
 
     return spec
 
